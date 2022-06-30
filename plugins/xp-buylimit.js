c651cdad4552c1711111ac893a1fa6df4c1b42a1
ILHAMBISAMAR <73697931+ILHAMBISAMAR@users.noreply.github.com>
2022-06-30 16:34:38 +0700

    Update xp-buylimit.js

--- plugins/xp-buylimit.js
+++ plugins/xp-buylimit.js
@@ -1,34 +1,17 @@
-const xpperlimit = 350 
+const xpperlimit = 100000
 let handler = async (m, { conn, command, args }) => {
-  let ftroli = {
-    key : {
-    remoteJid: '6283136505591-1614953337@g.us',
-    participant : '0@s.whatsapp.net'
-    },
-    message: {
-    orderMessage: {
-    itemCount : 2022,
-    status: 1,
-    surface : 1,
-    message: 'Get Limit', 
-    orderTitle: `Hyzer`,
-    thumbnail: 'https://telegra.ph/file/5ecbec3e82e247671a18e.jpg', 
-    sellerJid: '0@s.whatsapp.net' 
-    }
-    }
-    }
   let count = command.replace(/^buy/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
   if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
     global.db.data.users[m.sender].exp -= xpperlimit * count
     global.db.data.users[m.sender].limit += count
-    conn.reply(m.chat, `Berhasil Membeli ${count} Limit 🏷 Dengan ${xpperlimit * count} Xp 🧬`, ftroli) 
-  } else conn.reply(m.chat, `XP anda tidak mencukupi untuk membeli ${count} limit, Anda dapat menambah exp dengan bermain game dan rpg`, ftroli) 
+    conn.reply(m.chat, `-${xpperlimit * count} XP\n+ ${count} Limit`, m)
+  } else conn.reply(m.chat, `XP tidak mencukupi untuk membeli ${count} limit`, m)
 }
-handler.help = ['buy<jumlah limit>', 'buy <jumlah limit>', 'buyall']
+handler.help = ['buy <jumlah limit>', 'buyall']
 handler.tags = ['xp']
-handler.command = /^buylimit([0-9]+)|buylimit|buyalllimit$/i
+handler.command = /^buy([0-9]+)|buy|buyall$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
