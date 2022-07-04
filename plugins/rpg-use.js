08ac58d69770498a383b4a8ae206d3f5d5b46b8b
ILHAMBISAMAR <73697931+ILHAMBISAMAR@users.noreply.github.com>
2022-07-04 16:19:24 +0700

    Update rpg-use.js

--- plugins/rpg-use.js
+++ plugins/rpg-use.js
@@ -1,20 +1,35 @@
-let handler = async (m, { args, usedPrefix }) => {
+let { MessageType } = require('@adiwajshing/baileys')
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) =>  {
+	if (!db.data.chats[m.chat].rpg && m.isGroup) throw global.rpg
+    let msgerror = (pickRandom(['Error', 'astagfirullah error', 'Nice Error', 'Salah format keknya :v', 'error bro', 'Kocak error :v', 'wtf error :v', 'Ciaaa error', 'error cuyy', 'dahlah (emot batu) error']))
+    try {
+        let msgkurang = (pickRandom(['potionmu tidak cukup', 'ciaa gk cukup potionyya :v', 'wtf gk cukup :v', 'beli potion dulu, potionmu gk cukup', 'Duaarr potionmu gk cukup', 'eyyyy potionmu kurang', 'beli dulu lah, masak mau pakai potion tapi gk ada potionnnya :v', 'minta ke orang lain suruh transfer potion, biar potionmu gk kurang :v', 'Beli potion dulu KK']))
+        let msgpenuh = (pickRandom(['Nyawamu sudah penuh', 'coba deh liat inv mu, nyawamu kan dah 100 ngapai ngunain potion lagi?', 'health mu dah penuh woyy', 'ws kebek weh :v', 'nyawamu dah penuh :v', 'udh weh, udh penuh']))
     let healt = global.db.data.users[m.sender].healt
-    if (user.health >= 200) return m.reply(`
-Your ❤️health is full!
-`.trim())
-    const heal = 50 
-    let count = Math.max(1, Math.min(Number.MAX_SAFE_INTEGER, (isNumber(args[0]) && parseInt(args[0]) || Math.round((200 - user.health) / heal)))) * 1
-    if (user.potion < count) return m.reply(`
-Your 🧃Potion is not enough, you only have *${user.potion}* 🧃Potion
-type *${usedPrefix}buy potion ${count - user.potion}* to buy 🧃Potion
-`.trim())
-    user.potion -= count * 1
-    user.health += heal * count
-    m.reply(`
-Successful use of *${count}* 🧃Potion(s)
-`.trim())
-}
+        if (/use|pakai/i.test(command)) {
+            try {
+                let count = (/[0-9]/g.test(args[1])) ? !args[1] || args.length < 2 ? Math.max((Math.ceil((100 - global.db.data.users[m.sender].healt) / usepotion)), 1) : Math.max(args[1], 1) : Math.max((Math.ceil((100 - global.db.data.users[m.sender].healt) / usepotion)), 1)
+                 let msgsucces = (pickRandom(['success memakai', 'Nice succes menggunakan', 'berhasil meminum ', 'primitif anda menggunakan', 'anda memakai', 'Anda menggunakan']) + ' *' + (count * 1) + '* Potion')
+                 if (args[0] === 'potion') {
+                    if (global.db.data.users[m.sender].healt < 100) {
+                        if (global.db.data.users[m.sender].potion >= count * 1) {
+                            global.db.data.users[m.sender].potion -= count * 1
+                            global.db.data.users[m.sender].healt += usepotion * count
+                            conn.reply(m.chat, msgsucces, m)
+                        } else conn.reply(m.chat, msgkurang, m)
+                    } else conn.reply(m.chat, msgpenuh, m)
+                } else if (args.length > 2 && args[0] === !'potion') m.reply(pickRandom(['Hanya bisa menggunakan potion', 'Mau ngunain apa? Cuma bisa gunain potion :v', 'Wih mau gunain apa kamu, kan hanya bisa potion', 'Waduheck, hanya bisa potion', 'lah, mau gunain apa?, kan hanya bisa potion']) + '\nContoh penggunaan: *' + usedPrefix + 'potion 1*')
+            } catch (e) {
+                console.log(e)
+                m.reply(msgerror)
+                if (DevMode) {
+                    let file = require.resolve(__filename)
+                    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
+                        conn.sendMessage(jid, file + ' error\nNo: *' + m.sender.split`@`[0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', MessageType.text)
+                    }
+                }
+            }
+         }
 
 handler.help = ['heal']
 handler.tags = ['rpg']
